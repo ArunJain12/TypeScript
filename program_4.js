3ee5a047fa04b5a06b8503e59b86e48326516d99
ArunJain12 <39081167+ArunJain12@users.noreply.github.com>
2022-07-29 14:13:44 +0530

    Delete program_4.js

--- program_4.js
+++
@@ -1,7 +0,0 @@
-/*
-    Template Literals.
-*/
-var userId = "Arun";
-var password = 'Admin';
-var finalValue = "Given data Is :-- User ID: ".concat(userId, " & Password: ").concat(password);
-console.log(finalValue);
