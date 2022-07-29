f383dc7cec70137c83a6615425e7346176fcead8
ArunJain12 <39081167+ArunJain12@users.noreply.github.com>
2022-07-29 14:13:19 +0530

    Delete program_2.js

--- program_2.js
+++
@@ -1,9 +0,0 @@
-/*
-    Data Types.
-*/
-var nData = 10;
-var strData = "arun";
-var bData = true;
-console.log("Numeric Data: " + nData);
-console.log("String Data: " + strData);
-console.log("Boolean Data: " + bData);
