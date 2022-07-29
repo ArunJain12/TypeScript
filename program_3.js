ecd53b8bd5f9d3d373b695daf5ff86636f22c389
ArunJain12 <39081167+ArunJain12@users.noreply.github.com>
2022-07-29 14:13:35 +0530

    Delete program_3.js

--- program_3.js
+++
@@ -1,8 +0,0 @@
-/*
-    Using for-of Loop.
-*/
-var strArr = ["1", "2", "3", "4"];
-for (var _i = 0, strArr_1 = strArr; _i < strArr_1.length; _i++) {
-    var value = strArr_1[_i];
-    console.log(value);
-}
